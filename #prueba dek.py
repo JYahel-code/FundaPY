81a9f47b9598ca392dfbabfadaa84b2ddc62551b
JYahel-code <78939292+JYahel-code@users.noreply.github.com>
2024-02-21 12:35:32 -0500

    Add files via upload

---
+++ #prueba dek.py
@@ -0,0 +1,10 @@
+#prueba dek
+x1=float(input("ingrese la coordenada y x del  p"))
+x2=float(input("ingrese la coordenada y x del p"))
+y1=float(input("ingrese la coordenada y y del p"))
+y2=float(input("ingrese la coordenada y y del p"))
+d=0#distancia
+#procesos
+d=((x2-x1)==2+(y2-y1)==2)==(1/2)
+#salir
+print("la distancia es", d)
